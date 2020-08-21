38f6ddf5a2c59c8d7a6a3295171f5bb97f6e317b
mateomh <mmojicah.84@gmail.com>
2020-08-21 15:47:30 -0500

    Add bad tests for the my_all method

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -10,28 +10,63 @@ describe Enumerable do
   # end
 
   # Good cases
-  describe "My all" do
+  describe "My all method - good cases" do
     let(:case1) { %w[ant bear cat] }
     let(:case2) { [1, 2i, 3.14] }
-    let(:case3) { [nil, true, 99] }
+    let(:case3) { ['a' 'a'] }
     let(:case4) { [1, 1] }
     let(:case5) { [] }
 
-    it "Checks all the elements in case1 to be 3 letters in length" do
+    it "Checks for a block passed with no argument" do
       expect(case1.my_all? { |word| word.length >= 3 }).to eql(case1.all? { |word| word.length >= 3 })
     end
 
-    it "Checks all the elements in case2 to be numbers" do
+    it "Checks for REGEXP as an argument" do
+      expect(case1.my_all? (/a/)).to eql(case1.all? (/a/))
+    end
+
+    it "Checks for argument with a class and no block" do
       expect(case2.my_all? (Numeric)).to eql(case2.all? (Numeric))
     end
 
-    it "Checks all the elements in case 4 to be the number 1" do
+    it "Checks a string as an argument" do
+      expect(case3.my_all? ('a')).to eql(case3.all? ('a'))
+    end
+
+    it "Checks for numeric argument with no block passed" do
       expect(case4.my_all? (1)).to eql(case4.all? (1))
     end
 
-    it "Checks for empty array to work" do
+    it "Checks for empty array with no argument and not block passed" do
       expect(case5.my_all?).to eql(case5.all?)
     end
+  end
 
+  # Bad cases
+  describe "My all method - bad cases" do
+    let(:case1) { %w[ant bear cat] }
+    let(:case2) { ['a' 'b'] }
+    let(:case3) { [nil, true, 99] }
+    let(:case4) { [1, 2] }
+
+    it "Checks for a block passed with no argument" do
+      expect(case1.my_all? { |word| word.length >= 4 }).to eql(case1.all? { |word| word.length >= 4 })
+    end
+
+    it "Checks for REGEXP as an argument no block" do
+      expect(case1.my_all? (/t/)).to eql(case1.all? (/t/))
+    end
+
+    it "Checks a string as an argument" do
+      expect(case2.my_all? ('a')).to eql(case2.all? ('a'))
+    end
+
+    it "Checks when the input has different types insed it" do
+      expect(case3.my_all?).to eql(case3.all?)
+    end
+
+    it "Checks for different numbers in the array" do
+      expect(case4.my_all? (1)).to eql(case4.all? (1))
+    end
   end
 end
