cd1e4e53a1551a4fd19b13bdc394fc1e6735ea40
mateomh <mmojicah.84@gmail.com>
2020-08-21 18:17:08 -0500

    Fix linter errors

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,7 +1,3 @@
-# frozen_string_literal: true
-
-# rubocop:disable Metrics/BlockLength
-
 require './lib/enumerable.rb'
 
 describe Enumerable do
@@ -258,4 +254,3 @@ describe Enumerable do
     end
   end
 end
-# rubocop:enable Metrics/BlockLength
